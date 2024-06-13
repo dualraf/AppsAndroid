27bca6ef87364982d15a377036917c62fa671c39
dualraf <zairhaziel@gmail.com>
2024-06-13 18:44:16 -0500

    app con icon

--- AfirmacionesApp/app/src/main/java/com/example/afirmacionesapp/MainActivity.kt
+++ AfirmacionesApp/app/src/main/java/com/example/afirmacionesapp/MainActivity.kt
@@ -6,10 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,10 +26,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.afirmacionesapp.data.Datasource
 import com.example.afirmacionesapp.model.Affirmation
 import com.example.afirmacionesapp.ui.theme.AfirmacionesAppTheme
 
@@ -44,7 +54,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationsApp() {
-
+    val layoutDirection = LocalLayoutDirection.current
+    Surface (
+        Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateEndPadding(layoutDirection),
+            )
+    ){
+        AffirmationsList(affirmationList = Datasource().loadAffirmations())
+    }
 }
 
 @Composable
@@ -54,7 +79,8 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
             Image(
                 painter = painterResource(affirmation.imageResourceId),
                 contentDescription = stringResource(affirmation.stringResourceId),
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .height(194.dp),
                 contentScale = ContentScale.Crop
             )
@@ -62,6 +88,18 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
                 text = LocalContext.current.getString(affirmation.stringResourceId),
                 modifier = Modifier.padding(16.dp),
                 style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
+@Composable
+fun AffirmationsList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier){
+    LazyColumn (modifier = modifier){
+        items(affirmationList){affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
             )
         }
     }
