d124ab011ade13bed87dc68227bb7330c6941d5f
dualraf <zairhaziel@gmail.com>
2024-07-04 00:11:26 -0500

    WoofApp

---
+++ WoofApp/app/src/main/java/com/example/woofapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.woofapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.woofapp.ui.theme.WoofAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WoofAppTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    WoofApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WoofApp(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun WoofPreview() {
+    WoofAppTheme(darkTheme=false) {
+        WoofApp()
+    }
+}
