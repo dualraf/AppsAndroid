3f387a245f7b10098f57b7ae8e508bda90f42d9a
dualraf <zairhaziel@gmail.com>
2024-07-08 15:20:05 -0500

    shapes

--- WoofApp/app/src/main/java/com/example/woofapp/MainActivity.kt
+++ WoofApp/app/src/main/java/com/example/woofapp/MainActivity.kt
@@ -14,17 +14,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose.WoofAppTheme
 import com.example.woofapp.data.Dog
 import com.example.woofapp.data.dogs
-import com.example.woofapp.ui.theme.WoofAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,20 +47,22 @@ class MainActivity : ComponentActivity() {
 fun WoofApp(modifier: Modifier = Modifier) {
     LazyColumn {
         items(dogs){
-            DogItem(dog = it)
+            DogItem(dog = it, modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)))
         }
     }
 }
 
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier){
-    Row (
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(R.dimen.padding_small))
-    ){
-        DogIcon(icon = dog.imageResourceId)
-        DogInformation(name = dog.name, age = dog.age)
+    Card (modifier = modifier){
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_small))
+        ){
+            DogIcon(icon = dog.imageResourceId)
+            DogInformation(name = dog.name, age = dog.age)
+        }
     }
 }
 
@@ -66,8 +72,12 @@ fun DogIcon(
     modifier: Modifier = Modifier
 ){
     Image(
-        modifier = modifier.size(dimensionResource(R.dimen.image_size)),
+        modifier = modifier
+            .size(dimensionResource(id = R.dimen.image_size))
+            .padding(dimensionResource(id = R.dimen.padding_small))
+            .clip(MaterialTheme.shapes.small),
         painter = painterResource(icon),
+        contentScale = ContentScale.Crop,
         contentDescription = null)
 }
 
